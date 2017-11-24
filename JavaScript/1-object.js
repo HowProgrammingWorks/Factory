ebeb8f0de499d588b5bb7916d9d62a2eb870032a
tshemsedinov <timur.shemsedinov@gmail.com>
2017-11-24 05:48:09 +0000

    Udpate object factory

--- JavaScript/1-object.js
+++ JavaScript/1-object.js
@@ -1,20 +1,23 @@
 'use strict';
 
-function userFactory(name, group, email) {
+function userFactory1(name, group, email) {
   return { name, group, email };
 }
 
-/*
-
-const userFactory = (name, group, email) => ({
+const userFactory2 = (name, group, email) => ({
   name, group, email
 });
 
-const userFactory = (name, group, email) => {
+const userFactory3 = (name, group, email) => {
   name, group, email
 };
 
-*/
-
-const user1 = userFactory('marcus', 'emperors', 'marcus@spqr.it');
+const user1 = userFactory1('marcus', 'emperors', 'marcus@spqr.it');
 console.log(user1);
+
+const user2 = userFactory2('marcus', 'emperors', 'marcus@spqr.it');
+console.log(user2);
+
+const user3 = userFactory3('marcus', 'emperors', 'marcus@spqr.it');
+console.log(user3);
+// Explain: why undefined
