4cfd69739e447ec34baf43d44ce7b497c7182f22
tshemsedinov <timur.shemsedinov@gmail.com>
2017-11-27 07:40:49 +0000

    Aff functional example

---
+++ JavaScript/4-function.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const logger = (level = 'info') => {
+  const colors = {
+    warning: '\x1b[1;33m',
+    error: '\x1b[0;31m',
+    info: '\x1b[1;37m'
+  };
+  const color = colors[level] || colors.info;
+  return s => {
+    const date = new Date().toISOString();
+    console.log(color + date + '\t' + s);
+  };
+};
+
+const warning = logger('warning');
+warning('Hello');
