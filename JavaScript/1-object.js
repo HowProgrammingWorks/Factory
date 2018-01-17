77cd28d660069fca1c74cd774ac30dd8abdc3daa
tshemsedinov <timur.shemsedinov@gmail.com>
2018-01-17 02:36:50 +0200

    More factory examples

--- JavaScript/1-object.js
+++ JavaScript/1-object.js
@@ -12,6 +12,10 @@ const userFactory3 = (name, group, email) => {
   name, group, email
 };
 
+const userFactory4 = (name, group, email) => (
+  name, group, email
+);
+
 const user1 = userFactory1('marcus', 'emperors', 'marcus@spqr.it');
 console.log(user1);
 
@@ -21,3 +25,7 @@ console.log(user2);
 const user3 = userFactory3('marcus', 'emperors', 'marcus@spqr.it');
 console.log(user3);
 // Explain: why undefined
+
+const user4 = userFactory4('marcus', 'emperors', 'marcus@spqr.it');
+console.log(user4);
+// Explain: why 'marcus@spqr.it'
