b1f12d018c8dd37e2febdb9af6c64d74f746bdff
ser <ser@localhost.localdomain>
2020-01-05 22:04:18 +0200

    Loop for moved out from constructor of class Logable

--- JavaScript/3-class.js
+++ JavaScript/3-class.js
@@ -1,35 +1,37 @@
 'use strict';
 
-const logable = fields => class Logable {
-  constructor(data) {
-    this.values = data;
-    for (const key in fields) {
-      Object.defineProperty(Logable.prototype, key, {
-        get() {
-          console.log('Reading key:', key);
-          return this.values[key];
-        },
-        set(value) {
-          console.log('Writing key:', key, value);
-          const def = fields[key];
-          const valid = (
-            typeof value === def.type &&
-            def.validate(value)
-          );
-          if (valid) this.values[key] = value;
-          else console.log('Validation failed:', key, value);
-        }
-      });
+const logable = fields => {
+  class Logable {
+    constructor(data) {
+      this.values = data;
+    }
+    toString() {
+      let result = this.constructor.name + '\t';
+      for (const key in fields) {
+        result += this.values[key] + '\t';
+      }
+      return result;
     }
   }
-
-  toString() {
-    let result = this.constructor.name + '\t';
-    for (const key in fields) {
-      result += this.values[key] + '\t';
-    }
-    return result;
+  for (const key in fields) {
+    Object.defineProperty(Logable.prototype, key, {
+      get() {
+        console.log('Reading key:', key);
+        return this.values[key];
+      },
+      set(value) {
+        console.log('Writing key:', key, value);
+        const def = fields[key];
+        const valid = (
+          typeof value === def.type &&
+          def.validate(value)
+        );
+        if (valid) this.values[key] = value;
+        else console.log('Validation failed:', key, value);
+      }
+    });
   }
+  return Logable;
 };
 
 // Usage
