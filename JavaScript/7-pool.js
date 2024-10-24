36696d84d6b9e3b9a205becd1a404e3bb3f844e6
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-10-24 11:11:59 +0300

    Refactor to avoid mixins

--- JavaScript/7-pool.js
+++ JavaScript/7-pool.js
@@ -1,33 +1,40 @@
 'use strict';
 
-const pool = (item) => {
-  pool.items = pool.items || new Array(10).fill(new Array(1000).fill(0));
+const POOL_SIZE = 1000;
 
-  if (item) {
-    pool.items.push(item);
-    console.log('Recycle item, count =', pool.items.length);
-    return;
-  }
-  const res = pool.items.pop() || new Array(1000).fill(0);
-
-  console.log('Get from pool, count =', pool.items.length);
-  return res;
+const pool = (factory) => {
+  const instance = factory();
+  const items = new Array(POOL_SIZE).fill(instance);
+  return (item) => {
+    let res = null;
+    if (item) {
+      items.push(item);
+      console.log('Recycle item, count =', items.length);
+    } else {
+      res = items.pop() || factory();
+      console.log('Get from pool, count =', items.length);
+    }
+    return res;
+  };
 };
 
 // Usage
 
-const a1 = pool();
+const factory = () => new Array(1000).fill(0);
+const arrays = pool(factory);
+
+const a1 = arrays();
 const b1 = a1.map((x, i) => i).reduce((x, y) => x + y);
 console.log(b1);
 
-const a2 = pool();
+const a2 = arrays();
 const b2 = a2.map((x, i) => i).reduce((x, y) => x + y);
 console.log(b2);
 
-pool(a1);
-pool(a2);
+arrays(a1);
+arrays(a2);
 
-const a3 = pool();
+const a3 = arrays();
 const b3 = a3.map((x, i) => i).reduce((x, y) => x + y);
 console.log(b3);
 
